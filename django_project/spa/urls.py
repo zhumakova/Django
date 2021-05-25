9e6f6086ae1685f7c37c18baec1150ea3ac2eeea
bzhumakova <bzhumakova@gmail.com>
2021-05-25 16:07:36 +0600

    create model classes for master and service

--- django_project/spa/urls.py
+++ django_project/spa/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage,services,masters
+
+
 urlpatterns=[
-    path('',homepage)
+    path('', homepage),
+    path('services/',services),
+    path('masters/',masters)
 ]
 
