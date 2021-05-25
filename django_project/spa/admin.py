bd923adeed34eb6c11eb8a31a8cef8003ffe6d71
bzhumakova <bzhumakova@gmail.com>
2021-05-25 16:01:05 +0600

    register service and master models in amin panel

--- django_project/spa/admin.py
+++ django_project/spa/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Service
+from .models import Service,Master
 # Register your models here.
-admin.site.register(Service)
+admin.site.register([Service,Master])
