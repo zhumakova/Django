16e65f875a847fc22c0b8bb25d23c54587d1c8e3
bzhumakova <bzhumakova@gmail.com>
2021-05-25 18:22:23 +0600

    create html file for user login page

--- django_project/spa/urls.py
+++ django_project/spa/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import homepage,services,masters
+from .views import *
 
 
 urlpatterns=[
-    path('', homepage),
+    path('', homepage,name="home"),
     path('services/',services),
-    path('masters/',masters)
+    path('masters/',masters),
+    path ('register/',register_page),
+    path ('login/',login_page,name='login'),
+    path('master_detail/<int:master_id>/',master_detail,name='master_detail')
+
 ]
 
--- django_project/spa/views.py
+++ django_project/spa/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Service
 from .models import Master
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login
 
 def homepage(request):
-    return HttpResponse ('This is my first django app! :)')
+    return HttpResponse ('Welcome!:)')
 
 def services(request):
     services=Service.objects.all()
@@ -13,3 +15,27 @@ def services(request):
 def masters(request):
     masters=Master.objects.all()
     return  render(request,'Masters.html',{'masters':masters})
+
+def register_page(request):
+    form=UserCreationForm()
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request,'register.html',{'form':form})
+
+def login_page(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        login(request,user)
+        return redirect('home')
+    return render(request,'login.html')
+
+def master_detail(request,master_id):
+    try:
+        master=Master.objects.get(id=master_id)
+    except Master.DoesNotExist:
+        return HttpResponse('404')
+    return render(request,'master_detail.html',{'master':master})
