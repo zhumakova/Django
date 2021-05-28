6dc1c87672c9b37f5f6e1c205195bce07f1e6551
bzhumakova <bzhumakova@gmail.com>
2021-05-28 18:47:36 +0600

    create form for RegisterForm

--- django_project/spa/forms.py
+++ django_project/spa/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
-
 class RegisterForm(UserCreationForm):
     name = forms.CharField()
     age = forms.IntegerField()
