fe707af173d96dd81c8d47af012638f987a03ede
bzhumakova <bzhumakova@gmail.com>
2021-06-01 18:49:12 +0600

    register paths for billing app

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('spa.urls')),
     path('',include('user_profile.urls')),
+    path('bill/',include('billing.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
