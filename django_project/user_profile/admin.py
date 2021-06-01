a5206f72cc249ac5cdb31a559ae6adf3109ba731
bzhumakova <bzhumakova@gmail.com>
2021-06-01 18:57:12 +0600

    create functions: cardCreate, incrementBalance,transactionPage

--- django_project/user_profile/admin.py
+++ django_project/user_profile/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register([Profile, Order])
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user','service','status','payment_method','date_created']
+    readonly_fields = ['date_created']
+admin.site.register(Order,OrderAdmin)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=['user','photo','name','age','email','order_count','wallet']
+    readonly_fields = ['order_count','wallet']
+
+admin.site.register(Profile,ProfileAdmin)
+
