e8fa615c18a16efe5d85e7c9622d9c3a11ce48b1
bzhumakova <bzhumakova@gmail.com>
2021-06-02 18:48:52 +0600

    add path for delete order and update order

--- django_project/spa/urls.py
+++ django_project/spa/urls.py
@@ -5,7 +5,7 @@ urlpatterns=[
     path('', homepage,name="home"),
     path('services/',services,name="services"),
     path('masters/',masters),
-    path ('register/',register_page),
+    path ('register/',register_page,name='register'),
     path ('login/',login_page,name='login'),
     path('master_detail/<int:master_id>/',master_detail,name='master_detail')
 
