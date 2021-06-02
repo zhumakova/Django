e39d327a56dfe3219fd789f2c01be0286bf36bb5
bzhumakova <bzhumakova@gmail.com>
2021-06-02 18:49:04 +0600

    add path for delete order and update order

--- django_project/user_profile/urls.py
+++ django_project/user_profile/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
 from .views import *
+from django.utils import timezone
 
 urlpatterns = [
     path('profile/',profile_page),
     path('order/<int:service_id>/',order_page,name='order'),
     path('order/',my_orders,name='my_orders'),
-    path('delete_order/<int:order_id>/',delete_order)
+    path('delete_order/<int:order_id>/',delete_order),
+    path('update_order/<int:order_id>/',update_order)
 ]
