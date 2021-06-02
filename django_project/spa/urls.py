19b0bb62492844300409c82003a3942ea947764a
bzhumakova <bzhumakova@gmail.com>
2021-06-02 18:53:49 +0600

    add name for register url

--- django_project/spa/urls.py
+++ django_project/spa/urls.py
@@ -8,6 +8,5 @@ urlpatterns=[
     path ('register/',register_page,name='register'),
     path ('login/',login_page,name='login'),
     path('master_detail/<int:master_id>/',master_detail,name='master_detail')
-
 ]
 
