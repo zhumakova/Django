f75a1236f9c69c08b672a75c54a0c4d4f36b0c5e
bzhumakova <bzhumakova@gmail.com>
2021-06-03 18:36:17 +0600

    add name to profile_name,delete_order,update_order

--- django_project/spa/urls.py
+++ django_project/spa/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns=[
     path('', homepage,name="home"),
     path('services/',services,name="services"),
-    path('masters/',masters),
+    path('masters/',masters,name="masters"),
     path ('register/',register_page,name='register'),
     path ('login/',login_page,name='login'),
     path('master_detail/<int:master_id>/',master_detail,name='master_detail')
