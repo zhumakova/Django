3dfc81dbd061a5ea3f1ddda7f72855acebdaecb0
bzhumakova <bzhumakova@gmail.com>
2021-06-04 17:31:52 +0600

    add path logout for logging out

--- django_project/spa/urls.py
+++ django_project/spa/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
     path('masters/',masters,name="masters"),
     path ('register/',register_page,name='register'),
     path ('login/',login_page,name='login'),
-    path('master_detail/<int:master_id>/',master_detail,name='master_detail')
+    path('master_detail/<int:master_id>/',master_detail,name='master_detail'),
+    path('logout/',logout_page,name='logout')
 ]
 
