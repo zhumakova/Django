5168b34e853aa0e667687a0a2aa35df1729cddaa
bzhumakova <bzhumakova@gmail.com>
2021-06-04 17:31:28 +0600

    add form for updating user's data

--- django_project/user_profile/services.py
+++ django_project/user_profile/services.py
@@ -19,8 +19,15 @@ def countMoney(profile, instance):
     price = instance.service.price
     if instance.payment_method == 'wallet':
         if profile.wallet >= price:
+            if profile.sale_amount>0:
+                price=price-(price*profile.sale_amount)
             profile.wallet -= price
             profile.save()
             instance.status = 'closed'
         else:
             raise ValidationError('no money')
+
+def count_sale(profile):
+    if profile.order_count>50:
+        profile.sale_amount=0.05
+        profile.save()
